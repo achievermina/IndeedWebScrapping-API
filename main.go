f7a09e4845889903532aa5ccb7ae3e07efeb26a5
achievermina <lmina820@gmail.com>
2020-02-25 12:59:59 -0500

    create API using mux

--- main.go
+++ main.go
@@ -1,12 +1,59 @@
 package main
 
 import (
-	"github.com/achievermina/IndeedWebScrapping-API/scrapper"
+	//"github.com/achievermina/IndeedWebScrapping-API/scrapper"
+	"net/http"
+	"log"
+	"github.com/gorilla/mux"
 )
 
+//type Handler interface {
+//	ServeHTTP(ResponseWrtier, *Request)
+//}
+
+//type server struct {}
+func get(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "get called"}`))
+}
+
+func post(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(`{"message": "post called"}`))
+}
+
+func put(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte(`{"message": "put called"}`))
+}
+
+func delete(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "delete called"}`))
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte(`{"message": "not found"}`))
+}
+
 func main() {
-	term :="python"
-	scrapper.Scrape(term)
+	//term :="python"
+	//scrapper.Scrape(term)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", get).Methods(http.MethodGet)
+	r.HandleFunc("/", post).Methods(http.MethodPost)
+	r.HandleFunc("/", put).Methods(http.MethodPut)
+	r.HandleFunc("/", delete).Methods(http.MethodDelete)
+	r.HandleFunc("/", notFound)
+	log.Fatalln(http.ListenAndServe(":8080", nil))
+
 }
 
 
