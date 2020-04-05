19a5c7c50da82b82b8169d18c38b395a5f32991b
achievermina <lmina820@gmail.com>
2020-04-05 06:19:16 -0400

    Implement gRPC server, need to update the scrapper.go functions based on jobObject struct

--- main.go
+++ main.go
@@ -2,64 +2,102 @@ package main
 
 import (
 	"log"
-	"net/http"
-	"github.com/achievermina/IndeedWebScrapping-API/scrapper"
-	"strings"
-	"fmt"
-	"encoding/json"
-	"github.com/gorilla/mux"
+	sc_server "github.com/achievermina/IndeedWebScrapping-API/scrapper"
+	"google.golang.org/grpc"
+	"net"
+	"context"
 )
+
+
+type Server struct {
+	sc_server.UnimplementedJobServiceServer
+}
+
+// proto file 에는 첫문장 capital 아니더라도 여기서는 Must 첫 letter capital
+func (s *Server) Search(ctx context.Context, searchTerm *sc_server.SearchRequest) (*sc_server.SearchResponse) {
+	log.Printf("get tasks search term: %v", searchTerm.Term)
+	var jobs []*sc_server.JobObject
+	job1 := &sc_server.JobObject{
+		Id: "1",
+		Title: "programmer",
+		Location: "NY",
+		Salary: "$1000000",
+		Summary: "ASAP",
+	}
+	jobs = append(jobs, job1)
+	job2 := &sc_server.JobObject{
+		Id: "2",
+		Title: "programmer2",
+		Location: "NY",
+		Salary: "$1000000",
+		Summary: "ASAP",
+	}
+	jobs = append(jobs, job2)
+
+	return &sc_server.SearchResponse{Jobs: jobs}
+}
+
+func main() {
+	//gRPC
+	grpcServer := grpc.NewServer()
+	sc_server.RegisterJobServiceServer(grpcServer, &Server{})
+	listen, err := net.Listen("tcp", "0.0.0.0:3000")
+	if err != nil {
+		log.Fatalf("could not listen to 0.0.0.0:3000 %v", err)
+	}
+	log.Println("Server starting...")
+	log.Fatal(grpcServer.Serve(listen))
+}
+
 
 //https://medium.com/the-andela-way/build-a-restful-json-api-with-golang-85a83420c9da
 
-func home(w http.ResponseWriter, r *http.Request) {
-	//
-	//term, ok := r.URL.Query()["term"]  //query string --> GET
-	//
-	//if !ok || len(term[0]) < 1 {
-	//	log.Println("Url Param 'term' is missing")
-	//	return
-	//}
-
-	term := "python"
-	fmt.Println(term)
-
-	searchTerm := strings.ToLower(scrapper.ClearText(term))
-	res := scrapper.Scrape(searchTerm)
-	fmt.Println(res)
-
-}
-
-
-
-func main() {
-	term := "python"
-	fmt.Println(term)
-	searchTerm := strings.ToLower(scrapper.ClearText(term))
-	res := scrapper.Scrape(searchTerm)
-	// http response --> excel download
-	//http api response excel
-
-	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	log.Fatalln(http.ListenAndServe(":8080", r))
-
-	err = json.NewDecoder(r.Body).Decode(&res)
-	newsJson, err := json.Marshal(news)
-	if err != nil {
-		panic(err)
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(newsJson)
-
-
-	//http.HandleFunc("/", home)
-	//http.ListenAndServe(":8080", nil)
-}
-
-
+//func home(w http.ResponseWriter, r *http.Request) {
 //
+//	term, ok := r.URL.Query()["term"]  //query string --> GET
+//
+//	if !ok || len(term[0]) < 1 {
+//		log.Println("Url Param 'term' is missing")
+//		return
+//	}
+//
+//	term := "python"
+//	fmt.Println(term)
+//
+//	searchTerm := strings.ToLower(scrapper.ClearText(term))
+//	res := scrapper.Scrape(searchTerm)
+//	fmt.Println(res)
+//
+//}
+
+
+
+//func main() {
+//	term := "python"
+//	fmt.Println(term)
+//	searchTerm := strings.ToLower(scrapper.ClearText(term))
+//	res := scrapper.Scrape(searchTerm)
+//	// http response --> excel download
+//	//http api response excel
+//
+//	r := mux.NewRouter()
+//	r.HandleFunc("/", home)
+//	log.Fatalln(http.ListenAndServe(":8080", r))
+//
+//	err = json.NewDecoder(r.Body).Decode(&res)
+//	newsJson, err := json.Marshal(news)
+//	if err != nil {
+//		panic(err)
+//	}
+//	w.Header().Set("Content-Type", "application/json")
+//	w.WriteHeader(http.StatusAccepted)
+//	w.Write(newsJson)
+//
+//
+//	//http.HandleFunc("/", home)
+//	//http.ListenAndServe(":8080", nil)
+//}
+
 //type event struct {
 //	userID          string `json:"userID"`
 //	Title       string `json:"Title"`
